b04901c74515c5cd1b88db9e239173c09e6d95b4
Ittipat Pattum <nampu@finema.co>
2025-10-25 20:16:21 +0700

    save

--- Thai.ID/SceneDelegate.swift
+++ Thai.ID/SceneDelegate.swift
@@ -1,22 +1,14 @@
-//
-//  SceneDelegate.swift
-//  Thai.ID
-//
-//  Created by Pu on 25/10/2568 BE.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +39,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
--- Thai.ID/ViewController.swift
+++ Thai.ID/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Thai.ID
-//
-//  Created by Pu on 25/10/2568 BE.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +5,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
 }
-
