62321bafe004346732bc23811b087b8cc45e4d6e
Ittipat Pattum <nampu@finema.co>
2025-10-25 19:27:48 +0700

    Initial Commit

---
+++ Thai.ID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thai.ID
+//
+//  Created by Pu on 25/10/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
